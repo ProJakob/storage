dd77587f08fa0cda23e215a4e6666d987b2943f4
ProJakob <70058737+ProJakob@users.noreply.github.com>
2024-01-31 21:06:14 +0100

    Update boot-dante.sh

--- boot-dante.sh
+++ boot-dante.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 apt update
-apt install dante-server
+apt install -y dante-server screen
 
-danted -f dante-eth0.conf -p dante-eth0.pid -D
-danted -f dante-eth1.conf -p dante-eth1.pid -D
-danted -f dante-eth2.conf -p dante-eth2.pid -D
+rm -r configs >> /dev/null
+rm -r pids >> /dev/null
+
+mkdir configs
+mkdir pids
+
+for i in $(ls /sys/class/net | grep eth); do
+    echo "Creating interface config: $i"
+    curl -s https://raw.githubusercontent.com/ProJakob/storage/main/dante.conf | sed 's\REPLACE_INTERFACE\'"$i"'\g' | tee "configs/dante-$i.conf" >> /dev/null
+    touch "pids/dante-$i.pid"
+    screen -dmS "dante-$i" sh -c "danted -f 'configs/dante-$i.conf' -p 'pids/dante-$i.pid'; exec bash"
+    sleep 0.5  # Add a short delay to ensure the screen session is created before checking its status
+done
